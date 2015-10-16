755281f3a2e95f005d4c0d48580e754fbd2c9a2e
conannewww <261923538@qq.com>
2015-10-16 15:25:29 +0800

    3DTouchTest

---
+++ 3DTouchTest/3DTouchTest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  3DTouchTest
+//
+//  Created by YFL on 15/10/16.
+//  Copyright © 2015年 text. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    /*
+     typedef enum UIApplicationShortcutIconType : NSInteger { UIApplicationShortcutIconTypeCompose, UIApplicationShortcutIconTypePlay, UIApplicationShortcutIconTypePause, UIApplicationShortcutIconTypeAdd, UIApplicationShortcutIconTypeLocation, UIApplicationShortcutIconTypeSearch, UIApplicationShortcutIconTypeShare, UIApplicationShortcutIconTypeProhibit, UIApplicationShortcutIconTypeContact, UIApplicationShortcutIconTypeHome, UIApplicationShortcutIconTypeMarkLocation, UIApplicationShortcutIconTypeFavorite, UIApplicationShortcutIconTypeLove, UIApplicationShortcutIconTypeCloud, UIApplicationShortcutIconTypeInvitation, UIApplicationShortcutIconTypeConfirmation, UIApplicationShortcutIconTypeMail, UIApplicationShortcutIconTypeMessage, UIApplicationShortcutIconTypeDate, UIApplicationShortcutIconTypeTime, UIApplicationShortcutIconTypeCapturePhoto, UIApplicationShortcutIconTypeCaptureVideo, UIApplicationShortcutIconTypeTask, UIApplicationShortcutIconTypeTaskCompleted, UIApplicationShortcutIconTypeAlarm, UIApplicationShortcutIconTypeBookmark, UIApplicationShortcutIconTypeShuffle, UIApplicationShortcutIconTypeAudio, UIApplicationShortcutIconTypeUpdate } UIApplicationShortcutIconType;
+     */
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
